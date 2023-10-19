6964a48b4338fed08ff127105a55401c33ebe185
CovarrubiasAaron <145801340+CovarrubiasAaron@users.noreply.github.com>
2023-10-19 11:28:55 -0700

    scripting assignment
    Created scene and completed my 1st scripting assignment

---
+++ SriptingProjectP3Covarrubias/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(keyCode.R)
+           
+           GetCompnent<Reenderer>().marial.color.red
+
+
+                if(Input.GetKeyDown(KeyCode.G))
+
+                GetComponent<Renderer>().material.color=Color.green
+            
+                if(Input.GetKeyDown(KeyCode.B))
+
+                GetComponent<Renderer>().materail.color=Color.blue 
+    }
+}
